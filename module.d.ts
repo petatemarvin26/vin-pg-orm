2ae7f668ed088d360df78f2729be4917c442d3f4
Marvin Petate <46154422+petatemarvin26@users.noreply.github.com>
2026-07-23 22:31:38 +0800

    Revert "release/1.0.0 (#1)" (#2)
    This reverts commit 837567b0c337658f39c4e368131add0b2092da02.

--- module.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '*.svg' {
-  const ReactComponent: React.ElementType<React.SVGAttributes<SVGElement>>;
-  const Svg: React.ReactNode;
-  export {ReactComponent};
-  export default Svg;
-}
-
-declare module '*.css';
